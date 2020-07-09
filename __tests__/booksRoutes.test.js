64009ad3f15860d3c16b35a3459b5736a6488091
Seth Lafollette <sethlafollette@t-Seth-MacBook-Pro.local>
2020-07-09 12:50:58 -0400

    finished testing

--- __tests__/booksRoutes.test.js
+++ __tests__/booksRoutes.test.js
@@ -76,7 +76,7 @@ describe("GET /books", function () {
     })
 })
 
-describe("GET /books", function () {
+describe("GET /books/:isbn", function () {
 
     beforeEach(async function () {
         await request(app)
@@ -93,13 +93,13 @@ describe("GET /books", function () {
             });
     });
 
-    test("Gets all books", async function () {
+    test("Get a single book", async function () {
         const res = await request(app)
-            .get("/books")
+            .get("/books/Testing")
 
         expect(res.statusCode).toBe(200);
         expect(res.body).toEqual({
-            "books": [{
+            "book": {
                 "isbn": "Testing",
                 "amazon_url": "amazon.com",
                 "author": "Test",
@@ -108,7 +108,80 @@ describe("GET /books", function () {
                 "publisher": "Cannon",
                 "title": "Title_Test",
                 "year": 2020
-            }]
+            }
         })
+    })
+})
+
+describe("PUT /books/:isbn", function () {
+
+    beforeEach(async function () {
+        await request(app)
+            .post("/books")
+            .send({
+                "isbn": "Testing",
+                "amazon_url": "amazon.com",
+                "author": "Test",
+                "language": "English",
+                "pages": 118,
+                "publisher": "Cannon",
+                "title": "Title_Test",
+                "year": 2020
+            });
+    });
+
+    test("Update a book", async function () {
+        const res = await request(app)
+            .put("/books/Testing")
+            .send({
+                "isbn": "Testing",
+                "amazon_url": "amazon.com",
+                "author": "Testing PUT",
+                "language": "English",
+                "pages": 118,
+                "publisher": "Cannon",
+                "title": "Title_Test_PUT",
+                "year": 2020
+            })
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({
+            "book": {
+                "isbn": "Testing",
+                "amazon_url": "amazon.com",
+                "author": "Testing PUT",
+                "language": "English",
+                "pages": 118,
+                "publisher": "Cannon",
+                "title": "Title_Test_PUT",
+                "year": 2020
+            }
+        })
+    })
+})
+
+describe("DELETE /books/:isbn", function () {
+
+    beforeEach(async function () {
+        await request(app)
+            .post("/books")
+            .send({
+                "isbn": "Testing",
+                "amazon_url": "amazon.com",
+                "author": "Test",
+                "language": "English",
+                "pages": 118,
+                "publisher": "Cannon",
+                "title": "Title_Test",
+                "year": 2020
+            });
+    });
+
+    test("Delete a book", async function () {
+        const res = await request(app)
+            .delete("/books/Testing")
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ "message": "Book deleted" })
     })
 })
